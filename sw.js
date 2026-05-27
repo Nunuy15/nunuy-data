a1aac3bba4854fa40e8ca1cf29444039e3065dfa
Nurita Evitarina <vhithaalon@gmail.com>
2026-05-27 21:42:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,3 @@
-const CACHE='keuangan-v1';
-const ASSETS=['./','./index.html','./manifest.json','https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.5.0/dist/tabler-icons.min.css'];
-
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-      return res;
-    }).catch(()=>caches.match('./index.html')))
-  );
-});
+const CACHE = 'keuangan-v3';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
